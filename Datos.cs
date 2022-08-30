a026827bc44dc71b04e86ac995ca1b2bc279b261
unknown <arturo.megargel@cetys.edu.mx>
2022-08-29 20:29:53 -0700

    Instrucciones

---
+++ Datos.cs
@@ -0,0 +1,4 @@
+git status (revisar status verificaciones 
+git add (Agregar todos los archivos nuevos
+git commit-m"descripcion commit" (Poner una descripcion de los cambios realizados 
+git push(subir los campos
